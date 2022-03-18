d5aaec6396be56fd894155fe1dc70eec4ec19333
Viachaslau Viarbitski <viachaslau@vyachka.laptop>
2022-03-18 17:21:44 +0300

    moved out all getting/processing data from file functions

--- src/differ/Generator.php
+++ src/differ/Generator.php
@@ -2,25 +2,12 @@
 
 namespace Differ\Differ;
 
+use function Src\DataDecoding\Decoder\getJsonDecodedArray;
+
 function genDiff(string $path1, string $path2): string
 {
-    $json1 = readDataFromFile($path1);
-    $json2 = readDataFromFile($path2);
-
-    $decodedJson1 = json_decode($json1, true);
-    $decodedJson2 = json_decode($json2, true);
+    $decodedJson1 = getJsonDecodedArray($path1);
+    $decodedJson2 = getJsonDecodedArray($path2);
 
     return "it works!\n";
-}
-
-function readDataFromFile(string $filePath): string
-{
-    $file = fopen($filePath, 'r');
-    $data = '';
-
-    while (!feof($file)) {
-        $data .= fgets($file);
-    }
-
-    return $data;
 }
