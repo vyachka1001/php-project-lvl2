09bd52512a1c1d01317fcbc92c197e59a7668804
Viachaslau Viarbitski <viachaslau@vyachka.laptop>
2022-03-18 15:35:51 +0300

    add reading data from file and decoding json data into array

--- src/differ/Generator.php
+++ src/differ/Generator.php
@@ -2,7 +2,26 @@
 
 namespace Differ\Differ;
 
-function genDiff($path1, $path2): string
+function genDiff(string $path1, string $path2): string
 {
-    return 'it works!';
+    $json1 = readDataFromFile($path1);
+    $json2 = readDataFromFile($path2);
+
+    $decodedJson1 = json_decode($json1, true);
+    $decodedJson2 = json_decode($json2, true);
+
+    return "it works!\n";
+}
+
+function readDataFromFile(string $filePath): string
+{
+    $file = fopen($filePath, 'r');
+    $data = '';
+
+    while(!feof($file))
+    {
+        $data .= fgets($file);
+    }
+
+    return $data;
 }
