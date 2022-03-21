70ae7edff25f70e7828576a43d7b318b5c6d565a
Viachaslau Viarbitski <viachaslau@vyachka.laptop>
2022-03-21 14:46:02 +0300

    add PHPUnit to dev dependencies

---
+++ tests/src/dataDecoding/DecoderTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Src\DataDecoding;
+
+use PHPUnit\Framework\TestCase;
+use function Src\DataDecoding\Decoder\getJsonDecodedArray;
+
+class DecoderTest extends TestCase
+{
+    public function testGetJsonDecodedArray()
+    {
+        $jsonDecodedExpected = [
+            "host" => "hexlet.io",
+            "timeout" => 50,
+            "proxy" => "123.234.53.22",
+            "follow" => false
+        ];
+
+        $path = "tests/fixtures/file1.json";
+        $jsonDecodedActual = getJsonDecodedArray($path);
+        $this->assertEquals($jsonDecodedExpected, $jsonDecodedActual);
+    }
+}
+
