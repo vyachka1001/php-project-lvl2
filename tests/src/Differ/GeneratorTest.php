840c4e23500fde986b237b4daacfbe4bd287fce8
Viachaslau Viarbitski <viachaslau@vyachka.laptop>
2022-03-23 11:46:27 +0300

    add new test

--- tests/src/Differ/GeneratorTest.php
+++ tests/src/Differ/GeneratorTest.php
@@ -8,7 +8,10 @@ use function Differ\Differ\genDiff;
 
 class GeneratorTest extends TestCase
 {
-    public function testGenDiff()
+    private string $path1 = __DIR__ . "/../../fixtures/file1.json";
+    private string $path2 = __DIR__ . "/../../fixtures/file2.json";
+
+    public function testGenDiff1()
     {
         $diffExpected = [
             '- follow: false',
@@ -19,14 +22,33 @@ class GeneratorTest extends TestCase
             '+ verbose: true'
         ];
 
-        $expected = \implode("\n  ", $diffExpected);
-        $expected = "{\n  {$expected}\n}\n";
-
-        $path1 = __DIR__ . "/../../fixtures/file1.json";
-        $path2 = __DIR__ . "/../../fixtures/file2.json";
-
-        $actual = genDiff($path1, $path2);
+        $expected = $this::buildExpectedDiff($diffExpected);
+        $actual = genDiff($this->path1, $this->path2);
 
         $this->assertEquals($expected, $actual);
+    }
+
+    public function testGenDiff2()
+    {
+        $diffExpected = [
+            '+ follow: false',
+            '  host: \'hexlet.io\'',
+            '+ proxy: \'123.234.53.22\'',
+            '- timeout: 20',
+            '+ timeout: 50',
+            '- verbose: true'
+        ];
+
+        $expected = $this::buildExpectedDiff($diffExpected);
+        $actual = genDiff($this->path2, $this->path1);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    public static function buildExpectedDiff($diff): string
+    {
+        $expectedDiff = \implode("\n  ", $diff);
+
+        return "{\n  {$expectedDiff}\n}\n";
     }
 }
