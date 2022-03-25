b7c576b663ef01b93b46c4f99cc49da93d62a584
Viachaslau Viarbitski <viachaslau@vyachka.laptop>
2022-03-25 16:28:43 +0300

    add logic of output formatting

--- src/Differ/Generator.php
+++ src/Differ/Generator.php
@@ -2,28 +2,32 @@
 
 namespace Differ\Differ;
 
-use function Hexlet\Code\Parsers\JsonParser\getJsonParsedArray;
-use function Hexlet\Code\Parsers\YamlParser\getYamlParsedArray;
+use function Hexlet\Code\Parsers\JsonParser\getJsonParsedObject;
+use function Hexlet\Code\Parsers\YamlParser\getYamlParsedObject;
+use function Differ\DiffBuilder\buildDiffTree;
+use function Differ\DiffFormatter\formatDiffStr;
 
-function genDiff(string $path1, string $path2): string
+function genDiff(string $path1, string $path2, string $format = 'stylish'): string
 {
-    $parsed1 = getParsedArray($path1);
-    $parsed2 = getParsedArray($path2);
-    $keys1 = getSortedArrayKeys($parsed1);
-    $keys2 = getSortedArrayKeys($parsed2);
+    $parsed1 = getParsedObject($path1);
+    $parsed2 = getParsedObject($path2);
 
-    $diff = getDifferencesString($parsed1, $parsed2, $keys1, $keys2);
+    $obj_vars1 = \get_object_vars($parsed1);
+    $obj_vars2 = \get_object_vars($parsed2);
 
-    return "{{$diff}\n}\n";
+    $diffTree = buildDiffTree($obj_vars1, $obj_vars2);
+    $diffStr = formatDiffStr($diffTree, $format);
+
+    return $diffStr;
 }
 
-function getParsedArray(string $path): array
+function getParsedObject(string $path): object
 {
     $extension = getFileExtension($path);
     if ($extension === '.yaml' || $extension === '.yml') {
-        return getYamlParsedArray($path);
+        return getYamlParsedObject($path);
     } elseif ($extension === '.json') {
-        return getJsonParsedArray($path);
+        return getJsonParsedObject($path);
     }
 }
 
@@ -33,93 +37,4 @@ function getFileExtension(string $path): string
     $extension = \substr($path, $extensionPointInd);
 
     return $extension;
-}
-
-function getDifferencesString(array $parsed1, array $parsed2, array $keys1, array $keys2): string
-{
-    $index1 = 0;
-    $index2 = 0;
-    $length1 = \count($parsed1);
-    $length2 = \count($parsed2);
-    $signs = ['first' => '-', 'second' => '+', 'equal' => ' '];
-    $diff = '';
-
-    while ($index1 < $length1 && $index2 < $length2) {
-        $key1 = $keys1[$index1];
-        $key2 = $keys2[$index2];
-
-        $comparisonResult = \strcmp($key1, $key2);
-        if ($comparisonResult > 0) {
-            $diff .= createDifference($signs['second'], $key2, $parsed2);
-            $index2++;
-        } elseif ($comparisonResult < 0) {
-            $diff .= createDifference($signs['first'], $key1, $parsed1);
-            $index1++;
-        } else {
-            if ($parsed1[$key1] != $parsed2[$key2]) {
-                $diff .= createDifference($signs['first'], $key1, $parsed1);
-                $diff .= createDifference($signs['second'], $key2, $parsed2);
-            } else {
-                $diff .= createDifference($signs['equal'], $key1, $parsed1);
-            }
-
-            $index1++;
-            $index2++;
-        }
-    }
-
-    if ($index1 < $length1) {
-        $diff .= getRestOfData($parsed1, $index1, $signs['first']);
-    } elseif ($index2 < $length2) {
-        $diff .= getRestOfData($parsed2, $index2, $signs['second']);
-    }
-
-    return $diff;
-}
-
-function getSortedArrayKeys(array $arr): array
-{
-    $keys = array_keys($arr);
-    sort($keys);
-
-    return $keys;
-}
-
-function formKeyValueStr(string $key, array $decoded): string
-{
-    $value = getStringValueOfElement($decoded[$key]);
-    $result = $key . ": " . $value;
-
-    return $result;
-}
-
-function getStringValueOfElement($element): string
-{
-    if (\is_bool($element)) {
-        return var_export($element, true);
-    } else {
-        return \strval($element);
-    }
-}
-
-function createDifference(string $diffSign, string $key, array $decoded): string
-{
-    $str = formKeyValueStr($key, $decoded);
-    $diff = "\n  {$diffSign} {$str}";
-
-    return $diff;
-}
-
-function getRestOfData(array $decoded, int $startIndex, string $diffChar): string
-{
-    $length = count($decoded);
-    $keys = getSortedArrayKeys($decoded);
-    $diff = '';
-    for ($ind = $startIndex; $ind < $length; $ind++) {
-        $key = $keys[$ind];
-        $str = formKeyValueStr($key, $decoded);
-        $diff = "{$diff}\n  {$diffChar} {$str}";
-    }
-
-    return $diff;
 }
