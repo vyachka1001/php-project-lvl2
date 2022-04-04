d4449b872e2347c09ba0c05c6993584b117dea51
Viachaslau Viarbitski <viachaslau@vyachka.laptop>
2022-04-04 23:52:25 +0300

    rewrite return statement

--- src/Parsers/JsonParser.php
+++ src/Parsers/JsonParser.php
@@ -6,9 +6,5 @@ function getJsonParsedObject(string $path): object
 {
     $parsedJson = '';
     $jsonString = \file_get_contents($path);
-    if ($jsonString !== false) {
-        return \json_decode($jsonString);
-    }
-
-    return $parsedJson;
+    return \json_decode((string)$jsonString);
 }
