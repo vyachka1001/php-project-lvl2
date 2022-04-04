c7048649642b0674eb383b153f497e4509da1ac4
Viachaslau Viarbitski <viachaslau@vyachka.laptop>
2022-04-04 15:15:07 +0300

    impl added, deleted, updated values builders

--- src/Formatters/JsonFormatter.php
+++ src/Formatters/JsonFormatter.php
@@ -7,10 +7,18 @@ use Formatters\PlainFormatter;
 
 function formatInJson(array $tree): string
 {
-    $addedValues = buildChangedValues($tree, '+');
     $deletedValues = buildChangedValues($tree, '-');
+    $addedValues = buildChangedValues($tree, '+');
+    \print_r($addedValues);
 
-    return "abs";
+    $updatedValues = findIntersectionsOfArrays($addedValues, $deletedValues);
+    $deletedValues = findDifferencesOfArrays($deletedValues, $updatedValues);
+    $addedValues = findDifferencesOfArrays($addedValues, $updatedValues);
+
+    print_r($updatedValues);
+    \print_r($addedValues);
+
+    return createJsonFormattedString($deletedValues, $addedValues, $updatedValues);
 }
 
 function buildChangedValues(array $tree, string $sign = ' '): array
@@ -32,6 +40,49 @@ function buildChangedValues(array $tree, string $sign = ' '): array
     }, []);
 
     return $formattedTree;
+}
+
+function findIntersectionsOfArrays(array $array1, $array2): array
+{
+    $keys1 = \array_keys($array1);
+    $keys2 = \array_keys($array2);
+    $commonKeys = array_intersect($keys1, $keys2);
+
+    $intersections = \array_reduce($commonKeys, function ($acc, $item) use ($array1, $array2) {
+        if (\is_array($array1[$item]) && \is_array($array2[$item])) {
+            $acc[$item] = findIntersectionsOfArrays($array1[$item], $array2[$item]);
+            return $acc;
+        } else {
+            $acc[$item] = $array1[$item];
+            return $acc;
+        }
+    }, []);
+
+    return $intersections;
+}
+
+function findDifferencesOfArrays(array $array1, array $array2): array
+{
+    $keys1 = \array_keys($array1);
+    $keys2 = \array_keys($array2);
+    $commonKeys = array_intersect($keys1, $keys2);
+
+    $differences = \array_reduce($keys1, function ($acc, $item) use ($array1, $array2, $commonKeys) {
+        if (\in_array($item, $commonKeys, true)) {
+            if (is_array($array1[$item]) && is_array($array2[$item])) {
+                $possibleDiff = findDifferencesOfArrays($array1[$item], $array2[$item]);
+                if (!empty($possibleDiff)) {
+                    $acc[$item] = $possibleDiff;
+                }
+            }
+        } else {
+            $acc[$item] = $array1[$item];
+        }
+
+        return $acc;
+    }, []);
+
+    return $differences;
 }
 
 function createDifference(array $key): string
@@ -62,14 +113,7 @@ function createCurrNode(array $tree, int $spacesCount = 0): string
     return $formattedTree;
 }
 
-function createJsonFormattedString(array $deletedValues, array $addedValues): string
+function createJsonFormattedString(array $deletedValues, array $addedValues, array $updatedValues): string
 {
-    $spacesStep = 4;
-    $output = \str_repeat(' ', $spacesStep) . "deleted: [\n" . createCurrNode($deletedValues, $spacesStep) .
-        \str_repeat(' ', $spacesStep) . "]\n";
-
-    $output .= \str_repeat(' ', $spacesStep) . "added: [\n" . createCurrNode($addedValues, $spacesStep) .
-        \str_repeat(' ', $spacesStep) . "]\n";
-
-    return $output;
+    return "";
 }
